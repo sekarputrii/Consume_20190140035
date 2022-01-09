234f1e594b8a39f26deb64cbd92c7eba4632cbd4
sekarputrii <sekarputriretno83@gmail.com>
2022-01-09 17:17:36 +0700

    update

--- service.js
+++ service.js
@@ -15,6 +15,19 @@ function getAll(){
 
 }
 
+function getbyId(data){
+	const respon = axios.get("http://localhost:8080/data/barang", data)
+	// const dr = respon.then(resp => resp.data)
+	// return dr
+	.then((result) => {
+		console.log(result)
+		return result.data
+	}).catch((err) => {
+			console.error(err)
+	});
+
+}
+
 async function update(data){
 	await axios.put("http://localhost:8080/data/barang", data)
 	.then((result) => {
